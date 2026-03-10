f0ebf46598fe09c90ebe4bf6f73bcb754f61a6aa
Hugo DUMONT <hugo.dumont@saint-remi.net>
2026-03-10 16:38:07 +0100

    Projet 10/03/2026
    Ajout du formulaire FrmVisiteAjout et ouverture dédiée
    
    Création du formulaire FrmVisiteAjout (code, designer, resx) pour l'ajout de visites. Activation du bouton "programmerRendezVous" pour ouvrir ce formulaire. Adaptation de la méthode ouvrirFormulaire pour gérer l'affichage et la fermeture/masquage selon le type de formulaire.

--- Interface/FrmBase.cs
+++ Interface/FrmBase.cs
@@ -35,6 +35,10 @@ namespace Interface
         {
             // s'il ne reste qu'un formulaire en mémoire, cela signifie que l'utilisateur a fermé l'application
             // il faut alors quitter le thread de l'application pour éviter que le formulaire parent reste ouvert en arrière-plan
+            if (Application.OpenForms.Count == 1)
+            {
+                Application.ExitThread();
+            }
 
         }
 
@@ -45,12 +49,12 @@ namespace Interface
 
         private void programmerRendezVous_Click(object sender, EventArgs e)
         {
-            // ouvrirFormulaire(new FrmVisiteAjout(session));
+            ouvrirFormulaire(new FrmVisiteAjout(session));
         }
 
         private void modifierRendezVous_Click(object sender, EventArgs e)
         {
-            // ouvrirFormulaire(new FrmVisiteModification(session));
+            ouvrirFormulaire(new FrmVisiteModification(session));
         }
 
         private void imprimerRendezvous_Click(object sender, EventArgs e)
@@ -92,7 +96,14 @@ namespace Interface
         /// </summary>
         private void ouvrirFormulaire(Form frm)
         {
-           
+            frm.Show();
+
+            if (frm is FrmBase frmBase)
+            {
+                this.Hide();   
+            }
+            else
+                this.Close();
         }
 
         private void parametrerComposant()
