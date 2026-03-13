20ce09203f291a21b2c46e7417e84f69070b85d7
Hugo DUMONT <hugo.dumont@saint-remi.net>
2026-03-13 14:20:31 +0100

    13/03

--- Interface/FrmVisiteModification.Designer.cs
+++ Interface/FrmVisiteModification.Designer.cs
@@ -29,116 +29,155 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmVisiteModification));
-            panel2 = new Panel();
-            label5 = new Label();
-            label4 = new Label();
-            label3 = new Label();
+            panel5 = new Panel();
+            dtpDate = new DateTimePicker();
             label1 = new Label();
-            label2 = new Label();
-            panel3 = new Panel();
-            dateTimePicker1 = new DateTimePicker();
+            lbDate = new Label();
+            lbNom = new Label();
+            label3 = new Label();
             button1 = new Button();
+            label5 = new Label();
+            panelDroit = new Panel();
+            panel2 = new Panel();
+            dataGridView1 = new DataGridView();
+            label2 = new Label();
+            panel5.SuspendLayout();
+            panelDroit.SuspendLayout();
             panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // lblTitre
             // 
-            lblTitre.Size = new Size(800, 74);
+            lblTitre.Size = new Size(1160, 74);
             // 
-            // panel2
+            // panel5
             // 
-            panel2.Controls.Add(button1);
-            panel2.Controls.Add(dateTimePicker1);
-            panel2.Controls.Add(label5);
-            panel2.Controls.Add(label4);
-            panel2.Controls.Add(label3);
-            panel2.Controls.Add(label1);
-            panel2.Controls.Add(label2);
-            panel2.Dock = DockStyle.Right;
-            panel2.Location = new Point(524, 98);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(276, 307);
-            panel2.TabIndex = 13;
+            panel5.Controls.Add(dtpDate);
+            panel5.Controls.Add(label1);
+            panel5.Controls.Add(lbDate);
+            panel5.Controls.Add(lbNom);
+            panel5.Controls.Add(label3);
+            panel5.Controls.Add(button1);
+            panel5.Controls.Add(label5);
+            panel5.Location = new Point(34, 13);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(438, 403);
+            panel5.TabIndex = 7;
             // 
-            // label5
+            // dtpDate
             // 
-            label5.AutoSize = true;
-            label5.Location = new Point(22, 178);
-            label5.Name = "label5";
-            label5.Size = new Size(73, 15);
-            label5.TabIndex = 4;
-            label5.Text = "est remis au ";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(22, 135);
-            label4.Name = "label4";
-            label4.Size = new Size(165, 15);
-            label4.TabIndex = 3;
-            label4.Text = "Date et Heure du rendez vous ";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(22, 92);
-            label3.Name = "label3";
-            label3.Size = new Size(121, 15);
-            label3.TabIndex = 2;
-            label3.Text = "prévue initialement le";
+            dtpDate.Location = new Point(30, 244);
+            dtpDate.Name = "dtpDate";
+            dtpDate.Size = new Size(189, 23);
+            dtpDate.TabIndex = 5;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(22, 12);
+            label1.Location = new Point(30, 68);
             label1.Name = "label1";
             label1.Size = new Size(112, 15);
             label1.TabIndex = 0;
             label1.Text = "Le rendez vous chez";
             label1.Click += label1_Click;
             // 
-            // label2
+            // lbDate
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(22, 48);
-            label2.Name = "label2";
-            label2.Size = new Size(100, 15);
-            label2.TabIndex = 1;
-            label2.Text = "Nom du praticien";
+            lbDate.AutoSize = true;
+            lbDate.Location = new Point(30, 168);
+            lbDate.Name = "lbDate";
+            lbDate.Size = new Size(165, 15);
+            lbDate.TabIndex = 3;
+            lbDate.Text = "Date et Heure du rendez vous ";
+            lbDate.Click += label4_Click;
             // 
-            // panel3
+            // lbNom
             // 
-            panel3.Dock = DockStyle.Fill;
-            panel3.Location = new Point(0, 98);
-            panel3.Name = "panel3";
-            panel3.Size = new Size(524, 307);
-            panel3.TabIndex = 14;
+            lbNom.AutoSize = true;
+            lbNom.Location = new Point(30, 110);
+            lbNom.Name = "lbNom";
+            lbNom.Size = new Size(100, 15);
+            lbNom.TabIndex = 1;
+            lbNom.Text = "Nom du praticien";
+            lbNom.Click += label2_Click;
             // 
-            // dateTimePicker1
+            // label3
             // 
-            dateTimePicker1.Location = new Point(22, 209);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(200, 23);
-            dateTimePicker1.TabIndex = 5;
+            label3.AutoSize = true;
+            label3.Location = new Point(30, 138);
+            label3.Name = "label3";
+            label3.Size = new Size(121, 15);
+            label3.TabIndex = 2;
+            label3.Text = "prévue initialement le";
+            label3.Click += label3_Click;
             // 
             // button1
             // 
             button1.BackColor = Color.Red;
             button1.ForeColor = Color.Black;
-            button1.Location = new Point(157, 261);
+            button1.Location = new Point(205, 308);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(112, 23);
             button1.TabIndex = 6;
-            button1.Text = "Ajouter";
+            button1.Text = "Modifier";
             button1.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(30, 202);
+            label5.Name = "label5";
+            label5.Size = new Size(73, 15);
+            label5.TabIndex = 4;
+            label5.Text = "est remis au ";
+            // 
+            // panelDroit
+            // 
+            panelDroit.Controls.Add(panel5);
+            panelDroit.Dock = DockStyle.Right;
+            panelDroit.Location = new Point(656, 98);
+            panelDroit.Name = "panelDroit";
+            panelDroit.Size = new Size(504, 419);
+            panelDroit.TabIndex = 13;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(dataGridView1);
+            panel2.Controls.Add(label2);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(0, 98);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(656, 419);
+            panel2.TabIndex = 14;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(0, 15);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(656, 404);
+            dataGridView1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Dock = DockStyle.Top;
+            label2.Location = new Point(0, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(282, 15);
+            label2.TabIndex = 0;
+            label2.Text = "Selectionner la visite afin de modifier le rendez vous ";
+            label2.Enter += label2_Enter;
             // 
             // FrmVisiteModification
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(panel3);
+            ClientSize = new Size(1160, 562);
             Controls.Add(panel2);
+            Controls.Add(panelDroit);
             Icon = (Icon)resources.GetObject("$this.Icon");
             KeyPreview = true;
             Location = new Point(0, 0);
@@ -146,24 +185,31 @@
             Text = "FrmVisiteModification";
             Load += FrmVisiteModification_Load;
             Controls.SetChildIndex(lblTitre, 0);
+            Controls.SetChildIndex(panelDroit, 0);
             Controls.SetChildIndex(panel2, 0);
-            Controls.SetChildIndex(panel3, 0);
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            panelDroit.ResumeLayout(false);
             panel2.ResumeLayout(false);
             panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Panel panel2;
+        private Panel panel5;
+        private DateTimePicker dtpDate;
         private Label label1;
-        private Label label2;
-        private Panel panel3;
-        private Label label5;
-        private Label label4;
+        private Label lbDate;
+        private Label lbNom;
         private Label label3;
-        private DateTimePicker dateTimePicker1;
         private Button button1;
+        private Label label5;
+        private Panel panelDroit;
+        private Panel panel2;
+        private Label label2;
+        private DataGridView dataGridView1;
     }
 }
--- Interface/FrmVisiteModification.cs
+++ Interface/FrmVisiteModification.cs
@@ -25,5 +25,25 @@ namespace Interface
         {
 
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Enter(object sender, EventArgs e)
+        {
+
+        }
     }
 }
